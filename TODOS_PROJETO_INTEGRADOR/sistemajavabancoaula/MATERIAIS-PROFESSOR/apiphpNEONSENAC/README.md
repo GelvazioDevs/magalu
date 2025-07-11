cc5f30c16406e8679e2640715d1bef69e60922ec
Gelvaziodev <gelvazio@gmail.com>
2025-07-11 17:04:18 -0300

    OK

--- TODOS_PROJETO_INTEGRADOR/sistemajavabancoaula/MATERIAIS-PROFESSOR/apiphpNEONSENAC/README.md
+++
@@ -1,2 +0,0 @@
-# apiphp
-apiphp
--- TODOS_PROJETO_INTEGRADOR/sistemajavabancoaula/MATERIAIS-PROFESSOR/apiphpNEONSENAC/api/index.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-header("Access-Control-Allow-Origin:*");
-header('Access-Control-Allow-Credentials: false');
-header('Access-Control-Max-Age: 86400');
-date_default_timezone_set('America/Maceio');
-
-require_once('../vendor/autoload.php');
